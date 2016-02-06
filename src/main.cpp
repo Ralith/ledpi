2ef5735d139125b2810d5cfacb930e82edc5b00c
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-02-06 12:54:19 -0800

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
+#include <limits>
 
 #include "pigpio.h"
-
 #include "Uv.h"
 
 using namespace common;
@@ -31,30 +31,43 @@ void static_buffer_alloc_cb(size_t, uv_buf_t * buf) {
   buf->len = length;
 }
 
+using Power = uint8_t;
+constexpr Power POWER_MAX = std::numeric_limits<Power>::max();
+
+struct State {
+  Power power[4];
+};
+
+void apply(const State &state) {
+  for(int i = 0; i < 4; ++i) {
+    gpioPWM(pins[i].gpio, POWER_MAX - state.power[i]);
+  }
 }
 
-int main(int argc, char **argv) {
-  using namespace std::literals;
+}
 
+int main(int, char **) {
   if(gpioInitialise() < 0) {
     fprintf(stderr, "GPIO initialization failed\n");
     return 1;
   }
   GPIOGuard guard;
 
+  // Init and shut off all channels
   for(auto &pin : pins) {
     gpioSetMode(pin.gpio, PI_OUTPUT);
     gpioWrite(pin.gpio, true);
   }
 
+  // Default warm white
+  State state = {{POWER_MAX, POWER_MAX / 10, 0, POWER_MAX / 4}};
+  apply(state);
+
   uv::Loop loop;
   uv::UDP udp(loop);
-  uv::Timer timer(loop);
 
   auto shutdown_cb = [&](int){
     udp.close();
-    timer.close();
-
   };
 
   uv::Signal sigint(loop, shutdown_cb, SIGINT);
@@ -66,9 +79,7 @@ int main(int argc, char **argv) {
   uv_ip6_addr("::", 4242, &addr);
   udp.bind(reinterpret_cast<struct sockaddr *>(&addr));
   udp.recvStart(static_buffer_alloc_cb, [&](ssize_t result, const uv_buf_t *buf, const struct sockaddr *cAddr, unsigned flags) {
-      if(timer.active())
-        timer.stop();
-
+      (void)flags;
       if(result < 0) {
         fprintf(stderr, "read error: %s\n", uv_strerror(result));
         shutdown_cb(0);
@@ -76,29 +87,25 @@ int main(int argc, char **argv) {
       }
 
       if(result != 4) {
-        fprintf(stderr, "illegal %d-byte command received\n", result);
+        if(cAddr != nullptr) {
+          fprintf(stderr, "illegal %zd-byte command received\n", result);
+        }
         return;
       }
       printf("set:");
       for(int i = 0; i < 4; ++i) {
         printf(" %s=%d", pins[i].name, buf->base[i]);
-        gpioPWM(pins[i].gpio, buf->base[i]);
+        state.power[i] = POWER_MAX - buf->base[i];
       }
+      apply(state);
       printf("\n");
     });
-
-  unsigned i = 0;
-
-  timer.start([&](){
-      gpioWrite(pins[i].gpio, true);
-      i = (i + 1) % 4;
-      gpioWrite(pins[i].gpio, false);
-    },
-    0ms, 500ms);
 
   loop.run();
 
   sigint.close();
   sigterm.close();
+
+  // Cleanup iteration
   loop.run();
 }
